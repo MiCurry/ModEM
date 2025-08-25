0946d61f0f0918a9a519ed6ab5730c3dc0045f53
Miles Curry <2590700+MiCurry@users.noreply.github.com>
2025-08-25 19:22:46 +0000

    Fill out Implementation of EsolnManager

--- f90/ESolnManager.f90
+++ f90/ESolnManager.f90
@@ -2,16 +2,20 @@ module ESolnManager
 
     use GridDef
     use SolnSpace
+    use utilities
 
     implicit None
+
+    type (solnVectorMTX_t) :: EsMgr_eAll
 
     character(len=*), parameter :: FTYPE_ASCII = "ASCII"
     character(len=*), parameter :: FTYPE_BINARY = "BINARY"
     character(len=*), parameter :: FTYPE_NETCDF = "NETCDF"
     character(len=*), parameter :: FTYPE_HDF5 = "HDF5"
 
-
-    character(len=25) :: EsMgr_ftype = FTYPE_ASCII
+    type (grid_t), pointer :: EsMgr_grid => null()
+    character(len=25) :: EsMgr_ftype
+    character(len=256) :: EsMgr_prefix
     logical :: EsMgr_save_in_file
 
     public :: EsMgr_init
@@ -19,6 +23,7 @@ module ESolnManager
     public :: EsMgr_get
     public :: EsMgr_save
     public :: EsMgr_transfer
+    public :: FTYPE_ASCII, FTYPE_BINARY, FTYPE_NETCDF, FTYPE_HDF5
 
 contains
 
@@ -29,10 +34,65 @@ contains
 
         implicit none
 
-        type (grid_t), intent(in) :: grid
+        type (grid_t), target, intent(in) :: grid
         logical, intent(in), optional :: save_in_file
         character(len=*), intent(in), optional :: prefix
         character(len=*), intent(in), optional :: ftype
+
+        logical :: save_in_file_lcl
+        character(len=256) :: prefix_lcl
+        character(len=256) :: ftype_lcl
+
+        if (present(save_in_file)) then
+            save_in_file_lcl = save_in_file
+        else
+            save_in_file_lcl = .false.
+        end if
+
+        if (.not. save_in_file_lcl .and. present(prefix)) then
+            write(0,*) "Warning: Argument 'prefix' was passed, but 'save_in_file' was not present"
+            write(0,*) "Warning: 'prefix' will not have an effect. Set 'save_in_file' to true to save"
+            write(0,*) "Warning: esolns in files"
+        end if
+
+        if (.not. save_in_file_lcl .and. present(ftype)) then
+            write(0,*) "Warning: Argument 'ftype' was passed, but 'save_in_file' was not present"
+            write(0,*) "Warning: 'ftype' will not have an effect. Set 'save_in_file' to true to save"
+            write(0,*) "Warning: esolns in files"
+        end if
+
+        if (present(prefix)) then
+            prefix_lcl = prefix
+        else
+            prefix_lcl = "esoln"
+        end if
+
+        if (present(ftype)) then
+            ftype_lcl = ftype
+        else
+            ftype_lcl = FTYPE_ASCII
+        end if
+
+        select case (ftype_lcl)
+            case (FTYPE_ASCII : FTYPE_BINARY)
+            case (FTYPE_NETCDF)
+                write(0,*) "ERROR: NetCDF not implemented for storing esoln files"
+                write(0,*) "ERROR: Valid options are: [", trim(FTYPE_ASCII), " | ", trim(FTYPE_BINARY), "]"
+                call ModEM_abort()
+            case (FTYPE_HDF5)
+                write(0,*) "ERROR: HDF5 not implemented for storing esoln files"
+                write(0,*) "ERROR: Valid options are: [", trim(FTYPE_ASCII), " | ", trim(FTYPE_BINARY), "]"
+                call ModEM_abort()
+            case DEFAULT
+                write(0,*) "ERROR: ", trim(ftype_lcl), " is not a valid file type for Esoln"
+                write(0,*) "ERROR: Valid options are: [", trim(FTYPE_ASCII), " | ", trim(FTYPE_BINARY), "]"
+                call ModEM_abort()
+        end select
+
+        EsMgr_grid => grid
+        EsMgr_save_in_file = save_in_file_lcl
+        EsMgr_prefix = prefix_lcl
+        EsMgr_ftype = ftype_lcl
 
     end subroutine EsMgr_init
 
@@ -44,6 +104,8 @@ contains
         integer, intent(in) :: iTx
         integer, intent(in), optional :: nPol
 
+        call create_solnVector(EsMgr_grid, iTx, e, place_holder=EsMgr_save_in_file)
+
     end subroutine EsMgr_create_e
 
     subroutine EsMgr_create_eAll(eAll, nTx)
@@ -52,10 +114,19 @@ contains
 
         type (solnVectorMTX_t), intent(inout) :: eAll
         integer, intent(in) :: nTx
+        integer :: iTx
+
+        type(solnVector_t) :: e
+
+        call create_solnVectorMTX(nTx, eAll)
+
+        do iTx = 1, nTx
+            call create_solnVector(EsMgr_grid, iTx, e, place_holder=EsMgr_save_in_file)
+            call copy_solnVector(eAll % solns(iTx), e)
+        end do
 
     end subroutine EsMgr_create_eAll
     
-
     subroutine EsMgr_get(e, iTx, nPol)
 
         implicit none
@@ -64,17 +135,49 @@ contains
         integer, intent(in) :: iTx
         integer, intent(in), optional :: nPol
 
+        if (EsMgr_save_in_file) then
+            call read_esoln_from_file(e, iTx, nPol)
+        else
+            e = EsMgr_eAll % solns(iTx)
+        end if
+
     end subroutine EsMgr_get
 
+    subroutine read_esoln_from_file(e, iTx, nPol)
+
+        implicit none
+
+        type (solnVector_t), intent(inout) :: e
+        integer, intent(in) :: iTx
+        integer, intent(in), optional :: nPol
+
+        call read_solnVector(e, EsMgr_prefix, EsMgr_ftype)
+
+    end subroutine read_esoln_from_file
 
     subroutine EsMgr_save(e)
 
         implicit none
 
-        type (solnVector_t), intent(inout) :: e
+        type (solnVector_t), intent(in) :: e
+
+        if (EsMgr_save_in_file) then
+            call write_soln_to_file(e)
+        else
+            EsMgr_eAll % solns(e % tx)  = e
+        end if
 
     end subroutine EsMgr_save
 
+    subroutine write_soln_to_file(e)
+
+        implicit none
+
+        type (solnVector_t), intent(in) :: e
+
+        call write_solnVector(e, EsMgr_prefix, EsMgr_ftype)
+
+    end subroutine write_soln_to_file
 
     subroutine EsMgr_transfer(e, to, nPol)
 
